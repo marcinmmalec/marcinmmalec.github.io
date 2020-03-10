a1c4c86bf72bab6142ca7b097f32b7bb351b45e5
marcinmmalec <martinmmalec@gmail.com>
2020-03-10 16:39:08 -0600

    update
    update March 10th

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Add event listener for window resizing */
+window.addEventListener("resize", resizeNav);
+/* Function that changes which nav menu is displayed */
+function resizeNav () {
+    var w = document.documentElement.clientWidth;
+    //var h = document.documentElement.clientHeight;
+    //console.log("Window size: width= " + w + ", height= " + h);
+    /* If width is at most 800px, display the mobile nav bar, otherwise use the normal nav bar */
+    if (w <= 800) {
+        document.getElementById("nav_bar").style.display = "none";
+        document.getElementById("mobile_nav_bar").style.display = "block";
+    } else {
+        document.getElementById("nav_bar").style.display = "block";
+        document.getElementById("mobile_nav_bar").style.display = "none";
+    }
+}
+// Run the function once on page load
+resizeNav();
