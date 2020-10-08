f9fdf5dac42d09ad840083883d3e023a466ef2dc
marcinmmalec <martinmmalec@gmail.com>
2020-10-08 09:09:23 -0600

    test

---
+++ js/script2.js
@@ -0,0 +1,24 @@
+function closeBar() {
+    const navbar = document.getElementById('collapsibleNavbar');
+    if (navbar.classList.contains('show')) {
+        /*navbar.classList.remove('show');*/
+        $('.collapse').collapse('toggle');
+    }
+}
+
+window.addEventListener("resize", addResize) 
+
+function addResize() {
+    let x = document.getElementsByClassName('nav-link');
+    x = Array.from(x);
+    let w = document.documentElement.clientWidth;
+    if (w <= 767) {
+        x.forEach(a => {
+            a.setAttribute("onclick", 'closeBar()');
+        });
+    } else {
+        x.forEach(a => {
+            a.removeAttribute("onclick");
+        });
+    }
+}
